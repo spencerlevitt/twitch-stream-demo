fbf811be1f119b6585eb6f99029ac98ba8515398
Spencer Levitt <50083586+spencerlevitt@users.noreply.github.com>
2022-01-29 12:17:30 -0600

    Call createUser

--- src/routes/streamRoutes.js
+++ src/routes/streamRoutes.js
@@ -83,6 +83,20 @@ router.post("/email", async (req, res) => {
 
   );
 
+  const verificationId = await dotsFunctions.createUser(
+
+    email,
+
+    country,
+
+    phone,
+
+    first,
+
+    last,
+
+  );
+
 
   res.send("success");
 });
